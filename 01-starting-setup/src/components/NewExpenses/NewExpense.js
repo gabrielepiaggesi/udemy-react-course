f14b975fc86709d8572ea6686781d53187874398
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-05-06 17:52:42 +0200

    #57 - Handling Form Submission

--- 01-starting-setup/src/components/NewExpenses/NewExpense.js
+++ 01-starting-setup/src/components/NewExpenses/NewExpense.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = (props) => {
+const NewExpense = () => {
     return (
         <div className="new-expense">
             <ExpenseForm />
