959c2f9854f5026265c0baa153fbf594bd474a9c
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-05-09 18:23:42 +0200

    Practicing with lists

--- 01-starting-setup/src/components/Expenses/ExpensesList.js
+++ 01-starting-setup/src/components/Expenses/ExpensesList.js
@@ -11,17 +11,23 @@ function ExpensesList(props) {
         setFilteredYear(selectedYear);
     };
 
+    // this is not a state because depends already on the filteredYear stae, so if this state is updated, 
+    // the parent component (ExpensesList) is run again, so this function is run again with the new fileteredYear value 
+    // and so with resulting with a new array, so here the state for this array is not needed.
+    const filteredExpenses = props.items.filter(item => item.date.getFullYear().toString() === filteredYear);
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter year={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map((expense) => 
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date} 
-                />)}
+                {filteredExpenses.map((expense) => 
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date} 
+                    />
+                )}
             </Card>
         </div>
     )
