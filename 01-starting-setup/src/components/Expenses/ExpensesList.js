f99374f1cdd5d86745a537648959e827aa6644c3
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-05-10 12:02:36 +0200

    #67 Outputting conditional content

--- 01-starting-setup/src/components/Expenses/ExpensesList.js
+++ 01-starting-setup/src/components/Expenses/ExpensesList.js
@@ -16,18 +16,23 @@ function ExpensesList(props) {
     // and so with resulting with a new array, so here the state for this array is not needed.
     const filteredExpenses = props.items.filter(item => item.date.getFullYear().toString() === filteredYear);
 
+    let expensesContent = (<p>No expenses found.</p>);
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => 
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date} 
+            />
+        );
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter year={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map((expense) => 
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date} 
-                    />
-                )}
+                {expensesContent}
             </Card>
         </div>
     )
